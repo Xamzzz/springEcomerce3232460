bfa00a9c94933eb9ca7d7febcdc86005e9b8b816
Xamzzz <juanchos966@gmail.com>
2025-11-07 12:58:32 -0500

    v0.2_configuracionAplication.properties&conexionaBaseDeDatos_Xamzz_07112025

--- src/main/java/com/springecomerce/SpringEcomerceApplication.java
+++ src/main/java/com/springecomerce/SpringEcomerceApplication.java
@@ -2,9 +2,8 @@ package com.springecomerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SpringEcomerceApplication {
 
 	public static void main(String[] args) {
